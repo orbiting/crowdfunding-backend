1a173419ad3b70eef54058210d48d0729c77c60d
patte <p@tte.io>
2017-02-13 20:31:41 +0100

    add tables: crowdfundings, items, pledges, items_pledges and seed them

---
+++ migrations/1_cf.js
@@ -0,0 +1,58 @@
+
+exports.up = function(knex) {
+  return Promise.resolve(true).then( function() {
+    return knex.schema.createTable('crowdfundings', function(table) {
+      table.increments('id').primary()
+      table.string('name')
+      table.date('begin_date')
+      table.date('end_date')
+      table.integer('goal_people')
+      table.integer('goal_money')
+      table.timestamps()
+    })
+  }).then( function() {
+    return knex.schema.createTable('items', function(table) {
+      table.increments('id').primary()
+      table.string('name')
+      table.string('description')
+      table.integer('price')
+      table.integer('crowdfunding_id').references('id').inTable('crowdfundings')
+        .onUpdate('CASCADE').onDelete('CASCADE').notNullable()
+      table.timestamps()
+    })
+  }).then( function() {
+    return knex.schema.createTable('pledges', function(table) {
+      table.increments('id').primary()
+      table.integer('crowdfunding_id').references('id').inTable('crowdfundings')
+        .onUpdate('CASCADE').onDelete('CASCADE').notNullable()
+      table.integer('user_id').references('id').inTable('users')
+        .onUpdate('CASCADE').onDelete('CASCADE').notNullable()
+      table.integer('brutto')
+      table.boolean('payed')
+      table.timestamps()
+    })
+  }).then( function() {
+    return knex.schema.createTable('items_pledges', function(table) {
+      table.increments('id').primary()
+      table.integer('item_id').references('id').inTable('items')
+        .onUpdate('CASCADE').onDelete('CASCADE').notNullable()
+      table.integer('pledge_id').references('id').inTable('pledges')
+        .onUpdate('CASCADE').onDelete('CASCADE').notNullable()
+      table.integer('numItems')
+      table.timestamps()
+    })
+  }).then(function() {
+    console.log("migration done!")
+  }).catch(function(error) {
+    console.log("migration error:")
+    console.log(error)
+  })
+}
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTable('items_pledges')
+    .dropTable('pledges')
+    .dropTable('items')
+    .dropTable('crowdfundings')
+}
---
+++ seeds/1_cf.js
@@ -0,0 +1,71 @@
+exports.seed = function(knex, Promise) {
+  return Promise.all([
+    knex('items_pledges').del(),
+    knex('pledges').del(),
+    knex('items').del(),
+    knex('crowdfundings').del()
+  ]).then( () => {
+    return knex('crowdfundings').insert({
+      name: "all or nothing",
+      begin_date: new Date("2017-02-13T17:28:14.740Z"),
+      end_date: new Date("2017-03-13T23:59:59.000Z"),
+      goal_people: 3000,
+      goal_money: 75000000,
+      created_at: new Date(),
+      updated_at: new Date()
+    }).returning('id')
+  }).then( (crowdfundingId) => {
+    crowdfundingId = parseInt(crowdfundingId)
+    return knex('users').where('email', 'patrick.recher@project-r.construction').select('id').then( (userId) => {
+      userId = parseInt(userId[0].id)
+      console.log("crowdfundingId", crowdfundingId)
+      console.log("userId", userId)
+      return knex('pledges').insert({
+        crowdfunding_id: crowdfundingId,
+        user_id: userId,
+        brutto: 0,
+        payed: false,
+        created_at: new Date(),
+        updated_at: new Date()
+      }).returning('id').then( (pledgeId) => {
+        pledgeId = parseInt(pledgeId)
+        return Promise.all([
+          knex('items').insert({
+            crowdfunding_id: crowdfundingId,
+            name: "one membership",
+            description: "get one membership for yourself!",
+            price: 20000,
+            created_at: new Date(),
+            updated_at: new Date()
+          }).returning('id'),
+          knex('items').insert({
+            crowdfunding_id: crowdfundingId,
+            name: "get one, give one",
+            description: "give others a membership too!",
+            price: 40000,
+            created_at: new Date(),
+            updated_at: new Date()
+          }).returning('id')
+        ]).then( (itemIds) => {
+          itemIds = itemIds.map( (i) => { return parseInt(i[0]) } )
+          return Promise.all([
+            knex('items_pledges').insert({
+              item_id: itemIds[0],
+              pledge_id: pledgeId,
+              numItems: 1,
+              created_at: new Date(),
+              updated_at: new Date()
+            }),
+            knex('items_pledges').insert({
+              item_id: itemIds[1],
+              pledge_id: pledgeId,
+              numItems: 2,
+              created_at: new Date(),
+              updated_at: new Date()
+            })
+          ])
+        })
+      })
+    })
+  })
+}
