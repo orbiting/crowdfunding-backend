cd8b8775750189db4e5be6e6654cad127027111c
patte <p@tte.io>
2017-02-28 14:13:12 +0100

    correct indent

--- graphql/loaders.js
+++ graphql/loaders.js
@@ -2,13 +2,13 @@ require('dotenv').config()
 const DataLoader = require('dataloader')
 
 module.exports = (pgdb) => {
-	return {
-		crowdfundings: new DataLoader(async (keys) => {
-			return Promise.all(keys.map(async () => {
-				let table = pgdb['public']['crowdfundings']
-				const response = await table.find( {id: keys[0]} )
-				return response
-			}));
-		})
-	}
+  return {
+    crowdfundings: new DataLoader(async (keys) => {
+      return Promise.all(keys.map(async () => {
+        let table = pgdb['public']['crowdfundings']
+        const response = await table.find( {id: keys[0]} )
+        return response
+      }));
+    })
+  }
 }
