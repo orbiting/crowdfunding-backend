ffd1f4b5e9886d33222ce6f33e6e0b99992b45e2
patte <p@tte.io>
2017-03-05 14:13:51 +0100

    removed nice but unused global bigint key generator

--- migrations/sqls/20170227132625-init-crowdfunding-up.sql
+++ migrations/sqls/20170227132625-init-crowdfunding-up.sql
@@ -1,31 +1,8 @@
-
--- http://rob.conery.io/2014/05/29/a-better-id-generator-for-postgresql/
--- https://engineering.instagram.com/sharding-ids-at-instagram-1cf5a71e5a5c#.ybyftmp3q
---create sequence global_id_sequence;
---CREATE OR REPLACE FUNCTION id_generator(OUT result bigint) AS $$
---DECLARE
---    our_epoch bigint := 1314220021721;
---    seq_id bigint;
---    now_millis bigint;
---    -- the id of this DB shard, must be set for each
---    -- schema shard you have - you could pass this as a parameter too
---    shard_id int := 1;
---BEGIN
---    SELECT nextval('global_id_sequence') % 1024 INTO seq_id;
---
---    SELECT FLOOR(EXTRACT(EPOCH FROM clock_timestamp()) * 1000) INTO now_millis;
---    result := (now_millis - our_epoch) << 23;
---    result := result | (shard_id << 10);
---    result := result | (seq_id);
---END;
---$$ LANGUAGE PLPGSQL;
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 
 create table "crowdfundings" (
   "id"          uuid primary key not null default uuid_generate_v4(),
---	"id"          bigint primary key not null default id_generator(),
   "name"        varchar not null,
   "beginDate"   timestamptz not null,
   "endDate"     timestamptz not null,
