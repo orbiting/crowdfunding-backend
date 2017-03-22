2b2c87937b246eba0faf24a4a9e2d25447e4e01d
patte <p@tte.io>
2017-03-22 13:24:38 +0100

    big cleanup
    remove unused code
    remove client proxying
    mention DB setup in readme

--- README.md
+++ README.md
@@ -5,26 +5,21 @@ You need to have redis installed and running.
 
 Boostrap your .env file
 ```
-PORT=3001
-PUBLIC_URL=http://localhost:3001
-CLIENT_DEV_SERVER_URL=http://localhost:3002
-MAIL_URL=smtp://postmaster@sandboxc0bfac8e57f84f4880052f8899470727.mailgun.org:PASSWORD@smtp.mailgun.org/
-FROM_MAIL_ADDRESS=test_account@project-r.construction
-SESSION_REDIS_HOST=127.0.0.1
-SESSION_REDIS_PORT=6379
-PASSWORDLESS_REDIS_HOST=127.0.0.1
-PASSWORDLESS_REDIS_PORT=6379
-SESSION_SECRET='asdf'
-DATABASE_URL=postgres://postgres:PASSWORD@172.17.0.1:54321/postgres
+PORT=3003
+PUBLIC_URL=http://localhost:3003
+SESSION_SECRET=
+DATABASE_URL=postgres://postgres:postgres@172.17.0.1:54321/postgres
+MAILGUN_API_KEY=
+MAILGUN_DOMAIN=
+MAIL_FROM_ADDRESS=
 ```
 
-### Dependencies
-This project uses the @project-r/auth-server package. You have to be part of the @project-r npmjs.org team to be able to `npm install` it. You can also use them locally via `npm link`
+### DB
+This server requires access to a postgres database. Ensure the `DATABASE_URL` is correct.
+Bootstrap the DB like this:
 ```
-cd auth-server
-npm link
-cd ../cf_server
-npm link @project-r/auth-server
+db-migrate up
+npm run seed
 ```
 
 ### Run it
@@ -32,12 +27,3 @@ npm link @project-r/auth-server
 npm install
 npm start
 ```
-
-## Production
-TODO
-
-
-## Setup
-This server proxies requests to the cf_admin client (create-react-app). The setup is inspired by these docs:
-- https://www.fullstackreact.com/articles/using-create-react-app-with-a-server/
-- https://github.com/babel/example-node-server
--- index.js
+++ index.js
@@ -1,99 +1,43 @@
 const { PgDb } = require('pogi')
 const cors = require('cors')
 const express = require('express')
-//const authServer = require('@project-r/auth-server')
 require('dotenv').config()
-const graphql = require('./graphql')
-const raisenow = require('./src/raisenow')
 
 process.env.PORT = process.env.PORT || 3001
 
-//setup express
-const server = express()
+const auth = require('./src/auth')
+const graphql = require('./graphql')
+const raisenow = require('./src/raisenow')
 
-/*
-server.use(function(req, res, next) {
-  console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-  console.log("new request")
-  console.log(req)
-  console.log("----------------------------------------------------------")
-  next()
-})
-*/
 
-// Express only serves static assets in production
-if (process.env.NODE_ENV === 'production') {
-  server.use(express.static('dist/client_build'));
-}
+PgDb.connect({connectionString: process.env.DATABASE_URL}).then( (pgdb) => {
+  const server = express()
 
-//FIXME
-server.use('*', cors())
+	//FIXME
+  server.use('*', cors())
 
-// TODO cleanup
-//authServer.prepareDB( {dbUrl: process.env.USERS_DB_URL} )
-//const User = require('./models/user')
-
-//Promise.resolve(true).then((db) => {
-PgDb.connect({connectionString: process.env.DATABASE_URL}).then( pgdb  => {
-
-//  const authConfig = {
-//    //db: db,
-//    userModel: User,
-//    serverUrl: process.env.PUBLIC_URL,
-//    sessionRedisHost: process.env.SESSION_REDIS_HOST,
-//    sessionRedisPort: process.env.SESSION_REDIS_PORT,
-//    passwordlessRedisHost: process.env.PASSWORDLESS_REDIS_HOST,
-//    passwordlessRedisPort: process.env.PASSWORDLESS_REDIS_PORT,
-//    secret: process.env.SESSION_SECRET,
-//    mailUrl: process.env.MAIL_URL,
-//    mailFromAddress: process.env.MAIL_FROM_ADDRESS,
-//    mailSubject: 'Login to Project R',
-//    mailContent: function(link) {
-//      return `Ma’am, Sir,
-//
-//you can now access your account here:
-//${link}
-//
-//Your R-Crew`
-//    }
-//  }
-//  const { ensureLoggedIn, withUser } = authServer.configure(server, authConfig)
+  // Once DB is available, setup sessions and routes for authentication
+  auth.configure({
+    server: server,
+    users: pgdb['public']['users'],
+    secret: process.env.SESSION_SECRET,
+  })
 
   graphql(server, pgdb)
 
   raisenow(server, pgdb)
 
-  //server.get('/test', ensureLoggedIn, withUser, function(req, res) {
-  //  console.log("/test")
-  //  //console.log(res.locals.user)
-  //  res.end("you are logged in")
-  //})
-
-
-	// in DEV the client is run via react-scripts via a separate server
-	// requests that are not handled by this server have to be passed to the react-dev-server
-  if(process.env.NODE_ENV !== 'production') {
-    const httpProxy = require('http-proxy')
-    var proxy = httpProxy.createProxyServer({target: process.env.CLIENT_DEV_SERVER_URL, ws: true});
-
-
-    //proxy request
-    //server.use(function(req, res, next) {
-    //  console.log("proxy request to CREATE-REACT-APP for: "+req.originalUrl)
-    //  proxy.web(req, res)//, { target: 'http://localhost:3002' })
-    //})
-		// proxy WebSocket requests as well (webpackHotDevClient)
-		// (we don't use websockets for anything else than hot code reloading so far...)
-    // DOESN'T WORK :(
-    /*
-		server.on('upgrade', function (req, socket, head) {
-			proxy.ws(req, socket, head);
-		})
-    */
-  }
+  //FIXME remove
+  server.get('/test', function(req, res) {
+    if(req.user) {
+      res.end(JSON.stringify(req.user))
+    } else {
+      res.end("you are not logged in")
+    }
+  })
 
   // start the server
   server.listen(process.env.PORT, () => {
-    console.log('new server is running on http://localhost:'+process.env.PORT)
+    console.log('server is running on http://localhost:'+process.env.PORT)
   })
 })
