c8bea82b99ea3207d1707891cfce694ebdb9c97a
patte <p@tte.io>
2017-03-27 12:59:57 +0200

    fix user.name nullable not role.name

--- migrations/sqls/20170227114808-add-users-and-roles-up.sql
+++ migrations/sqls/20170227114808-add-users-and-roles-up.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 create table "users" (
   "id"          uuid primary key not null default uuid_generate_v4(),
   "email"       text not null unique check ("email" ~* '^.+@.+\..+$'),
-  "name"        varchar not null,
+  "name"        varchar,
   "verified"    boolean not null default false,
   "token"       varchar,
   "createdAt"   timestamptz default now(),
@@ -12,7 +12,7 @@ create table "users" (
 
 create table "roles" (
   "id"          uuid primary key not null default uuid_generate_v4(),
-  "name"        varchar,
+  "name"        varchar not null,
   "description" text not null,
   "createdAt"   timestamptz default now(),
   "updatedAt"   timestamptz default now()
