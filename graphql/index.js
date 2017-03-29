36f9407354bf43f97a13bab2c1abf5c7864f86f1
patte <p@tte.io>
2017-03-29 19:01:16 +0200

    add optics.apollodata.com

--- graphql/index.js
+++ graphql/index.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser')
 const {graphqlExpress, graphiqlExpress} = require('graphql-server-express')
 const {makeExecutableSchema} = require('graphql-tools')
+const OpticsAgent = require('optics-agent')
 
 const Schema = require('./schema')
 const Resolvers = require('./resolvers')
@@ -9,11 +10,18 @@ const executableSchema = makeExecutableSchema({
   typeDefs: Schema,
   resolvers: Resolvers
 })
-
 const createLoaders = require('./loaders')
+
+//agent for optics.apollodata.com
+OpticsAgent.configureAgent({
+  reportIntervalMs: 20*1000
+})
+OpticsAgent.instrumentSchema(executableSchema)
 
 
 module.exports = (server, pgdb) => {
+  server.use(OpticsAgent.middleware())
+
   server.use('/graphql',
     bodyParser.json(),
     graphqlExpress( (req) => {
@@ -25,6 +33,7 @@ module.exports = (server, pgdb) => {
         },
         schema: executableSchema,
         context: {
+          opticsContext: OpticsAgent.context(req),
           loaders: createLoaders(pgdb),
           pgdb,
           user: req.user,
