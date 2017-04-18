43afe110b44b7589d861a7965b5af11d8bdd709e
patte <p@tte.io>
2017-04-18 20:17:36 +0200

    remove faqs migrations and seeds

--- migrations/sqls/20170327090749-faq-up.sql
+++
@@ -1,9 +0,0 @@
-create type "faqStatus" as ENUM ('DRAFT', 'PUBLISHED');
-create table "faqs" (
-  "id"              uuid primary key not null default uuid_generate_v4(),
-  "status"          "faqStatus" not null default 'DRAFT',
-  "question"        text not null,
-  "answer"          text not null,
-  "createdAt"       timestamptz default now(),
-  "updatedAt"       timestamptz default now()
-);
