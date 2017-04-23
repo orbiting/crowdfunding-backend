ea0d5dd75c0f7a32a59b01c98c6a9a9ed55e5dc4
patte <p@tte.io>
2017-04-24 00:13:59 +0200

    add option to render smImages with user's names

--- script/generateSMImages.js
+++ script/generateSMImages.js
@@ -15,16 +15,22 @@ const renderUrl = require('../lib/renderUrl')
 
 const FOLDER = 'testimonials'
 const { ASSETS_BASE_URL, FRONTEND_BASE_URL, S3BUCKET } = process.env
+const WITH_USER_NAME = false
 
 PgDb.connect().then( async (pgdb) => {
 
   let counter = 0
-
   const testimonials = await pgdb.public.testimonials.find({})
 
   for(let testimonial of testimonials) {
 
-    const smImagePath = `/${FOLDER}/sm/${testimonial.id}_sm.png`
+    let smImagePath = `/${FOLDER}/sm/${testimonial.id}_sm.png`
+
+    if(WITH_USER_NAME) {
+      const user = await pgdb.public.users.findOne({id: testimonial.userId})
+      smImagePath = `/${FOLDER}/sm/${user.firstName}_${user.lastName}.png`
+    }
+
     const url = ASSETS_BASE_URL+smImagePath
 
     await renderUrl(`${FRONTEND_BASE_URL}/community?share=${testimonial.id}`, 1200, 628)
