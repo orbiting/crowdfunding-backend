786c16b2bcb503c41275835b47b95db63b4769f8
patte <p@tte.io>
2017-04-25 04:01:31 +0200

    cleanup paymentLog webhooks

--- src/paymentsLog.js
+++ src/paymentsLog.js
@@ -6,7 +6,7 @@ module.exports = (pgdb) => {
   //https://stripe.com/docs/webhooks
   server.post('/payments/stripe',
     bodyParser.json(),
-    async function(req, res) {
+    async (req, res) => {
       await pgdb.public.paymentsLog.insert({
         method: 'STRIPE',
         pspPayload: req.body
@@ -16,8 +16,8 @@ module.exports = (pgdb) => {
 
   // https://developer.paypal.com/docs/integration/direct/webhooks/rest-webhooks/
   server.post('/payments/paypal',
-    bodyParser.json(),
-    async function(req, res) {
+    bodyParser.urlencoded({extended: true}),
+    async (req, res) => {
       await pgdb.public.paymentsLog.insert({
         method: 'PAYPAL',
         pspPayload: req.body
@@ -26,14 +26,11 @@ module.exports = (pgdb) => {
   })
 
   //https://e-payment-postfinance.v-psp.com/de/guides/integration%20guides/e-commerce/transaction-feedback#servertoserver-feedback
-  server.post('/payments/pf', async function(req, res) {
-    console.log('--------------------------------')
-    console.log('/payments/pf')
-    console.log(req.body)
-    console.log(req.query)
+  server.post('/payments/pf', async (req, res) => {
+    bodyParser.urlencoded({extended: true}),
     await pgdb.public.paymentsLog.insert({
       method: 'POSTFINANCECARD',
-      pspPayload: req.query
+      pspPayload: req.body
     })
     return res.sendStatus(200)
   })
