a220c973a7867f75f16c4ed80210bfa7e1bed6ea
patte <p@tte.io>
2017-04-25 23:19:57 +0200

    cleanup script uploadImages

--- script/uploadImages.js
+++ script/uploadImages.js
@@ -8,32 +8,35 @@ const FOLDER = 'images'
 const IMAGES_DIR = __dirname+'/data/images/'
 const IMAGES_WIDTH = 1200
 
-const {S3BUCKET} = process.env
+const {S3BUCKET, ASSETS_BASE_URL} = process.env
 
 Promise.resolve().then( async () => {
   const files = await fs.readdir(IMAGES_DIR)
-  await Promise.all(files.map( async (file) => {
-    if(!(await fs.stat(IMAGES_DIR+file)).isDirectory()) {
 
-      const image = fs.readFileSync(IMAGES_DIR+file, 'binary')
+  let urls = []
+  for(let filename of files) {
+    if(filename[0] !== '.' &&
+      !(await fs.stat(IMAGES_DIR+filename)).isDirectory()) {
+
+      const image = fs.readFileSync(IMAGES_DIR+filename, 'binary')
       const inputBuffer = new Buffer(image, 'binary')
 
-      const filename = slugify(file.substr(0, file.lastIndexOf('.')))
-      const path = `/${FOLDER}/${filename}.jpeg`
-      console.log('uploading: '+path)
+      const slug = slugify(filename.substr(0, filename.lastIndexOf('.')))
+      const path = `/${FOLDER}/${slug}.jpeg`
+      urls.push(ASSETS_BASE_URL+path)
+      console.log('uploading: '+filename)
 
-      await Promise.all([
-        convertImage.toWidth(inputBuffer, IMAGES_WIDTH)
-          .then( async (data) => {
-            await uploadExoscale({
-              stream: data,
-              path: path,
-              mimeType: 'image/jpeg',
-              bucket: S3BUCKET
-            })
-          }),
-      ])
-
+      await convertImage.toWidth(inputBuffer, IMAGES_WIDTH)
+        .then( async (data) => {
+          await uploadExoscale({
+            stream: data,
+            path: path,
+            mimeType: 'image/jpeg',
+            bucket: S3BUCKET
+          })
+        })
     }
-  }))
+  }
+  console.log('\nHurrah! These are your new image urls:')
+  console.log(urls.join('\n'))
 })
