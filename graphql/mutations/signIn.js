77d8292977353ff1b65dcbdcf2f0d5eeae18e913
patte <p@tte.io>
2017-05-03 23:29:57 +0200

    signin: find existing email with different cases and send email to existing

--- graphql/mutations/signIn.js
+++ graphql/mutations/signIn.js
@@ -1,5 +1,5 @@
 const signIn = require('../../lib/signIn')
 
 module.exports = async (_, args, {pgdb, user, req, t}) => {
-  return signIn(args.email, args.context, req, t)
+  return signIn(args.email, args.context, pgdb, req, t)
 }
