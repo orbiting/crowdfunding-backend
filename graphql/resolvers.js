c55aae8fba0afd9668afbee95eb3dc94795c3ee2
patte <p@tte.io>
2017-05-15 22:05:38 +0200

    fix: a verified user without pledges can submit a new pledge without email verify so he must be able to retreive them later

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -83,6 +83,18 @@ const resolveFunctions = {
         const user = await pgdb.public.users.findOne({id: pledge.userId})
         if(!user.verified) {
           return pledge
+        } else {
+          //user not logged in, but verified. Return pledge only if draft and user has no non-draft pledges
+          //this is for:
+          //a verified user without pledges can submit a new pledge without email verify,
+          //so he must be able to retreive them later.
+          const hasNonDraftPledges = await pgdb.public.pledges.findFirst({
+            userId: user.id,
+            'status !=': 'DRAFT'
+          })
+          if(pledge.status === 'DRAFT' && !hasNonDraftPledges) {
+            return pledge
+          }
         }
       }
       return null
