131455069bdf605414d44d357439c57f5159c4a1
patte <p@tte.io>
2017-05-18 23:09:51 +0200

    log comments to slack

--- graphql/mutations/submitComment.js
+++ graphql/mutations/submitComment.js
@@ -1,6 +1,12 @@
 const ensureSignedIn = require('../../lib/ensureSignedIn')
 const logger = require('../../lib/logger')
 
+const {SLACK_API_TOKEN, SLACK_CHANNEL_VOTING} = process.env
+let SlackWebClient
+if(SLACK_API_TOKEN) {
+  SlackWebClient = new(require('@slack/client').WebClient)(SLACK_API_TOKEN)
+}
+
 module.exports = async (_, args, {pgdb, user, req, t, publish}) => {
   ensureSignedIn(req, t)
 
@@ -59,6 +65,16 @@ module.exports = async (_, args, {pgdb, user, req, t, publish}) => {
 
     await transaction.transactionCommit()
     await publish('commentAdded', comment)
+
+    if(SlackWebClient) {
+      const slackContent = `${user.firstName} ${user.lastName} wrote: (${comment.id.substring(0, 8)})\n\n${content}`
+      SlackWebClient.chat.postMessage(SLACK_CHANNEL_VOTING, slackContent, (err, res) => {
+        if (err) {
+          logger.error('slack error', {err})
+        }
+      })
+    }
+
   } catch(e) {
     await transaction.transactionRollback()
     logger.error('error in transaction', { req: req._log(), args, error: e })
