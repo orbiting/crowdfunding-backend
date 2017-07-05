bb4589f6ed51881e8cce1c6cb406152f8473de64
Lukas Buenger <lukasbuenger@gmail.com>
2017-07-05 14:21:02 +0200

    user enpoint incl. resolver

--- graphql/schema.js
+++ graphql/schema.js
@@ -10,6 +10,7 @@ schema {
 type RootQuerys {
   me: User
   users(limit: Int!, offset: Int, orderBy: String): Users!
+  user(id: String): User
 
   crowdfundings: [Crowdfunding]
   crowdfunding(name: String!): Crowdfunding!
