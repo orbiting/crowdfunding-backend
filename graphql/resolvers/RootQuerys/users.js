3aa21796ed94a670ef8ed3b517666d46fafdd57b
patte <p@tte.io>
2017-07-06 15:00:10 +0200

    users: with or without search

--- graphql/resolvers/RootQuerys/users.js
+++ graphql/resolvers/RootQuerys/users.js
@@ -13,39 +13,47 @@ module.exports = async (
   { pgdb, user }
 ) => {
   Roles.ensureUserHasRole(user, 'supporter')
-  const items = await pgdb.query(`
-    SELECT
-      u.*
-    FROM
-      users u
-    LEFT JOIN
-      addresses a
-      ON a.id = u."addressId"
-    LEFT JOIN
-      memberships m
-      ON m."userId" = u.id
-    WHERE
-      u."firstName" % :search OR
-      u."lastName" % :search OR
-      u.email % :search OR
-      a.name ILIKE :searchLike OR
-      a.line1 ILIKE :searchLike OR
-      a.line2 ILIKE :searchLike OR
-      a.city ILIKE :searchLike OR
-      a.country ILIKE :searchLike OR
-      m."sequenceNumber"::text ILIKE :searchLike
-    ORDER BY :orderBy
-    OFFSET :offset
-    LIMIT :limit;
-  `, {
-    search,
-    searchLike: search + '%',
-    limit,
-    offset,
-    orderBy: (orderBy && deserializeOrderBy(orderBy)) || {
-      createdAt: 'asc'
-    }
-  })
+  const items = !search
+    ? await pgdb.public.users.findAll({
+      limit,
+      offset,
+      orderBy: (orderBy && deserializeOrderBy(orderBy)) || {
+        createdAt: 'asc'
+      }
+    })
+   : await pgdb.query(`
+       SELECT
+         u.*
+       FROM
+         users u
+       LEFT JOIN
+         addresses a
+         ON a.id = u."addressId"
+       LEFT JOIN
+         memberships m
+         ON m."userId" = u.id
+       WHERE
+         u."firstName" % :search OR
+         u."lastName" % :search OR
+         u.email % :search OR
+         a.name ILIKE :searchLike OR
+         a.line1 ILIKE :searchLike OR
+         a.line2 ILIKE :searchLike OR
+         a.city ILIKE :searchLike OR
+         a.country ILIKE :searchLike OR
+         m."sequenceNumber"::text ILIKE :searchLike
+       ORDER BY :orderBy
+       OFFSET :offset
+       LIMIT :limit;
+     `, {
+       search,
+       searchLike: search + '%',
+       limit,
+       offset,
+       orderBy: (orderBy && deserializeOrderBy(orderBy)) || {
+         createdAt: 'asc'
+       }
+     })
   const count = await pgdb.public.users.count()
   return { items, count }
 }
