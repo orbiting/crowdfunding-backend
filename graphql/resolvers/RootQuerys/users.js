735c687058fffc50cb75abe02ff0d5cb5095f212
patte <p@tte.io>
2017-07-07 11:10:06 +0200

    users search with pg similarity

--- graphql/resolvers/RootQuerys/users.js
+++ graphql/resolvers/RootQuerys/users.js
@@ -23,55 +23,41 @@ module.exports = async (
     })
     : await pgdb.query(`
         SELECT
-          "id",
-          "firstName",
-          "lastName",
-          "email",
-          "verified",
-          "birthday",
-          "phoneNumber",
-          "addressId",
-          ts_rank_cd(textsearch, query) AS rank
-        FROM (
-          SELECT
-            u."id" as "id",
-            u."firstName" as "firstName",
-            u."lastName" as "lastName",
-            u."email" as "email",
-            u."verified" as "verified",
-            u."birthday" as "birthday",
-            u."phoneNumber" as "phoneNumber",
-            u."addressId" as "addressId",
-            to_tsvector(concat_ws(' ',
-              u."firstName"::text,
-              u."lastName"::text,
-              a.name::text,
-              a.line1::text,
-              a.line2::text,
-              a.city::text,
-              a.country::text,
-              m."sequenceNumber"::text
-          )) AS textsearch
-          FROM
-            users u
-          LEFT JOIN
-            addresses a
-            ON a.id = u."addressId"
-          LEFT JOIN
-            memberships m
-            ON m."userId" = u.id
-        ) as t1,
-          to_tsquery(:search) query
-        WHERE
-          query @@ textsearch
+          u.*,
+          concat_ws(' ',
+            u."firstName"::text,
+            u."lastName"::text,
+            u.email::text,
+            a.name::text,
+            a.line1::text,
+            a.line2::text,
+            a.city::text,
+            a.country::text
+          ) <->> :search AS word_sim,
+          concat_ws(' ',
+            u."firstName"::text,
+            u."lastName"::text,
+            u.email::text,
+            a.name::text,
+            a.line1::text,
+            a.line2::text,
+            a.city::text,
+            a.country::text
+          ) <-> :search AS dist
+        FROM
+          users u
+        LEFT JOIN
+          addresses a
+          ON a.id = u."addressId"
+        LEFT JOIN
+          memberships m
+          ON m."userId" = u.id
         ORDER BY
-          rank DESC
+          word_sim, dist
         OFFSET :offset
         LIMIT :limit
      `, {
-       // plainto_tsquery(:search) query
-       // search: search.trim(),
-       search: search.split(' ').join('|'),
+       search: search.trim(),
        limit,
        offset
      })
