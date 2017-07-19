c1d6f5a862d3bdb8cf9619b41559c4eebc341518
patte <p@tte.io>
2017-07-19 18:36:04 +0200

    filters for payments

--- graphql/resolvers/RootQuerys/payments.js
+++ graphql/resolvers/RootQuerys/payments.js
@@ -1,21 +1,22 @@
 const Roles = require('../../../lib/Roles')
+const {dateRangeFilterWhere, stringArrayFilterWhere, booleanFilterWhere} = require('../../../lib/Filters')
 const deserializeOrderBy = require('../../../lib/deserializeOrderBy')
+
+const searchWhere = (search, prefix) => {
+  if (!search) { return '' }
+  return `
+    ${prefix || ''}
+    (hrid ILIKE :search OR
+    "pspId" ILIKE :search)
+  `
+}
 
 module.exports = async (
   _,
-  { limit, offset, orderBy, search },
+  { limit, offset, orderBy, search, dateRangeFilter, stringArrayFilter, booleanFilter },
   { pgdb, user }
 ) => {
   Roles.ensureUserHasRole(user, 'supporter')
-
-  const paymentIds = search
-    ? (await pgdb.public.payments.findWhere(`
-        hrid ILIKE :search OR
-        "pspId" ILIKE :search
-      `, {
-        search: `${search}%`
-      })).map(payment => payment.id)
-    : null
 
   const options = {
     limit,
@@ -25,9 +26,20 @@ module.exports = async (
     }
   }
 
-  const items = paymentIds
-    ? await pgdb.public.payments.find({id: paymentIds}, options)
-    : await pgdb.public.payments.findAll(options)
+  const items = !(search || dateRangeFilter)
+    ? await pgdb.public.payments.findAll(options)
+    : await pgdb.public.payments.findWhere(`
+      ${searchWhere(search)}
+      ${dateRangeFilterWhere(dateRangeFilter, 'AND')}
+      ${stringArrayFilterWhere(stringArrayFilter, 'AND')}
+      ${booleanFilterWhere(booleanFilter, 'AND')}
+    `, {
+      search: `${search}%`,
+      fromDate: dateRangeFilter ? dateRangeFilter.from : null,
+      toDate: dateRangeFilter ? dateRangeFilter.to : null,
+      stringArray: stringArrayFilter ? stringArrayFilter.values : null,
+      booleanValue: booleanFilter ? booleanFilter.value : null
+    }, options)
 
   const count = await pgdb.public.payments.count()
   return { items, count }
