2f0a20fb0aee6fa40f53828d66355dba65410aad
patte <p@tte.io>
2017-07-19 11:24:09 +0200

    secure user query (requires role supporter)

--- graphql/resolvers/RootQuerys/user.js
+++ graphql/resolvers/RootQuerys/user.js
@@ -1,3 +1,6 @@
-module.exports = async (_, { id }, { pgdb }) => {
+const Roles = require('../../../lib/Roles')
+
+module.exports = async (_, { id }, { pgdb, user }) => {
+  Roles.ensureUserHasRole(user, 'supporter')
   return pgdb.public.users.findOne({ id })
 }
